f90442d1b7b9176acf129ab0e99eb433f304530e
Nahid <nahidhassan25@gmail.com>
2018-04-27 21:26:35 -0700

    FIxed commands and download visual studios code

--- installs.sh
+++ installs.sh
@@ -4,8 +4,11 @@ if [ "$(id -u)" != "0" ]; then
   exec sudo "$0" "$@"
 fi
 
-apt-get install update -y
-apt-get install upgrade -y
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+apt-get update
+apt-get upgrade -y
 apt-get install vim -y
 apt-get install gcc -y
 apt-get install gdb -y
@@ -17,6 +20,7 @@ apt-get install openjdk-11-jdk -y
 apt-get install openjdk-11-jre -y
 apt-get install ssh -y
 apt-get install dos2unix -y
+sudo apt-get install code
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mv .vimrc ..
 vim a -c 'PluginInstall' -c 'q' -c 'q'
