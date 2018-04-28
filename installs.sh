71e36c94fbc95ce0bbcc08e7cd8d3f57db91b949
Nahid <nahidhassan25@gmail.com>
2018-04-27 21:29:51 -0700

    added -y to vscode

--- installs.sh
+++ installs.sh
@@ -20,7 +20,7 @@ apt-get install openjdk-11-jdk -y
 apt-get install openjdk-11-jre -y
 apt-get install ssh -y
 apt-get install dos2unix -y
-sudo apt-get install code
+sudo apt-get install code -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mv .vimrc ..
 vim a -c 'PluginInstall' -c 'q' -c 'q'
