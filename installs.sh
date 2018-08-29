275fc65ab537aa58a4c4efea6335717a53be212f
Nahid <sadasd>
2018-08-29 19:52:18 -0400

    added color for tmux

--- installs.sh
+++ installs.sh
@@ -26,6 +26,7 @@ apt-get install sshpass -y
 apt-get install tmux -y
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ..
+echo "export TERM=xterm-256color" >> ~/.profile
 cd
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
