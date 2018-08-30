8d6604d9667315cfbf05943a3d73cf00b79ef90c
Nahid <sadasd>
2018-08-30 09:27:32 -0400

    removed terminator and added comments

--- installs.sh
+++ installs.sh
@@ -4,9 +4,11 @@ if [ "$(id -u)" != "0" ]; then
   exec sudo "$0" "$@"
 fi
 
+# Repo for vs code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
 apt-get update
 apt-get upgrade -y
 apt-get install vim -y
@@ -23,18 +25,19 @@ apt-get install openjdk-11-jre -y
 apt install net-tools -y
 apt-get install ssh -y
 apt-get install dos2unix -y
-apt-get install code -y
+apt-get install code -y     #Install vs code
 apt-get install sshpass -y
-apt-get install terminator -y
+apt-get install tmux -y
 #Install powerline for bash
 apt-get install fonts-powerline -y
 pip install powerline-shell
 cat bash.config >> ~/.bashrc
 
-#Install custom tmux settings
-apt-get install tmux -y
+#Vim settings
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ..
+
+#Install custom tmux settings
 echo "export TERM=xterm-256color" >> ~/.profile
 cd
 git clone https://github.com/gpakosz/.tmux.git
