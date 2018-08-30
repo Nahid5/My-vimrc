0175c0003bf49d9169c39830894e1230eab1a847
Nahid <sadasd>
2018-08-30 09:42:48 -0400

    script does not run everything as root anymore

--- installs.sh
+++ installs.sh
@@ -1,35 +1,31 @@
 #!/usr/bin/env bash
 
-if [ "$(id -u)" != "0" ]; then
-  exec sudo "$0" "$@"
-fi
-
 # Repo for vs code
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-apt-get update
-apt-get upgrade -y
-apt-get install vim -y
-apt-get install gcc -y
-apt-get install gdb -y
-apt-get install git -y
-apt-get install python -y
-apt-get install python-pip -y
-apt-get install python3 -y
-apt-get install python3-pip -y
-apt-get install curl -y
-apt-get install openjdk-11-jdk -y
-apt-get install openjdk-11-jre -y
-apt install net-tools -y
-apt-get install ssh -y
-apt-get install dos2unix -y
-apt-get install code -y     #Install vs code
-apt-get install sshpass -y
-apt-get install tmux -y
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install vim -y
+sudo apt-get install gcc -y
+sudo apt-get install gdb -y
+sudo apt-get install git -y
+sudo apt-get install python -y
+sudo apt-get install python-pip -y
+sudo apt-get install python3 -y
+sudo apt-get install python3-pip -y
+sudo apt-get install curl -y
+sudo apt-get install openjdk-11-jdk -y
+sudo apt-get install openjdk-11-jre -y
+sudo apt install net-tools -y
+sudo apt-get install ssh -y
+sudo apt-get install dos2unix -y
+sudo apt-get install code -y     #Install vs code
+sudo apt-get install sshpass -y
+sudo apt-get install tmux -y
 #Install powerline for bash
-apt-get install fonts-powerline -y
+sudo apt-get install fonts-powerline -y
 pip install powerline-shell
 cat bash.config >> ~/.bashrc
 
